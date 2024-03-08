83e5ff41945cc3d34135a1f750d11c34d538bfdf
Thomas <tcoates_1@yahoo.com>
2024-03-08 16:41:00 -0500

    remove card-header and add chirp-bar component

--- resources/views/components/profile/card.blade.php
+++ resources/views/components/profile/card.blade.php
@@ -5,7 +5,7 @@
             <img src="{{ $user->profile_image }}" alt="Current Profile Picture" class="md:h-24 md:w-24 h-12 w-12 object-cover rounded-full mr-3 transform hover:scale-125 min-w-10 min-h-10" onerror="this.onerror=null; this.src='{{ asset('images/Lake.jpg') }}'; this.alt='image default';">
         </a>
         <div>
-            <h1 class="card-header align-middle md:text-3xl text-blue-600 leading-tight hover:font-bold">
+            <h1 class="align-middle md:text-3xl text-blue-600 leading-tight hover:font-bold">
                 <a href="{{ route('profile.show', ['username' => $user->username]) }}">
                     {{ $user->username }}
                 </a>
@@ -13,7 +13,7 @@
         </div>
     </div>
     <div class="shrink justify-end col-start-2">
-        <h1 class="card-header text-right md:text-3xl text-blue-600 leading-tight hover:font-bold">
+        <h1 class="text-right md:text-3xl text-blue-600 leading-tight hover:font-bold">
             <a href="{{ route('profile.show', ['username' => $user->username]) }}">
                 {{ $user->name }}
             </a>
