1ca2859fed99c87d9c069b1cbc0d6d78929e512b
Thomas <tcoates_1@yahoo.com>
2024-03-28 09:35:51 -0400

    renames and restructures landing page into welcome folder

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Index');
+    return view('welcome/index');
 });
 
 Route::get('/dashboard', [ProfileController::class, 'dashboard'])
